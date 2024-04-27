486384a43ce63ac1881873dfac6bfeba676b3476
Miguel Angel <mp.ruesga@outlook.com>
2024-04-27 14:01:47 -0600

    updated stl_to_vtk.py

--- stl_to_vtk.py
+++ stl_to_vtk.py
@@ -1,16 +1,25 @@
 import sys
 import vtk
 
-# Read the .stl file
-filename = sys.argv[1]
-a = vtk.vtkSTLReader()
-a.SetFileName(filename)
-a.Update()
-a = a.GetOutput()
+import gmsh
+gmsh.initialize()
 
-# Write the .vtk file
-filename = filename.replace('.stl', '.vtk')
-b = vtk.vtkPolyDataWriter()
-b.SetFileName(filename)
-b.SetInputData(a)
-b.Update()
+stl = gmsh.merge('Brain stl/lh_simplified.stl')
+gmsh.model.mesh.classifySurfaces(gmsh.pi, True, True, gmsh.pi)
+gmsh.model.mesh.createGeometry()
+
+s = gmsh.model.getEntities(2)
+surf = gmsh.model.geo.addSurfaceLoop([e[1] for e in s])
+vol = gmsh.model.geo.addVolume([surf])
+gmsh.model.geo.synchronize()
+gmsh.model.occ.removeAllDuplicates()
+gmsh.option.setNumber('Mesh.Algorithm', 1)
+gmsh.option.setNumber('Mesh.MeshSizeMax', 5)
+gmsh.option.setNumber('Mesh.MeshSizeMin', 0.5)
+
+gmsh.model.addPhysicalGroup(3, [1], 1)
+gmsh.model.setPhysicalName(3, 1, "The volume")
+gmsh.model.occ.synchronize()
+gmsh.model.mesh.generate(3)
+gmsh.write("lh_simplified.vtk")
+gmsh.finalize()
