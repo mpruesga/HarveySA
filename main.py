c961bf6c8f454d41c712253c7ee0aa10c3c4951b
Miguel Angel <mp.ruesga@outlook.com>
2024-04-29 19:53:19 -0600

    added kinova arm

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ planeId = p.loadURDF("plane.urdf", [0,0,-2])
 startOrientation = p.getQuaternionFromEuler([0,0,math.pi])
 boxId = p.loadURDF("skull.urdf", [0,0,0],globalScaling=3.5, baseOrientation=startOrientation , useMaximalCoordinates = False, useFixedBase=1)
 
+RobotId = p.loadURDF("GEN3-6DOF_VISION_URDF_ARM_V01.urdf",[1,1,-2])
 
 bunnyId2 = p.loadSoftBody("torus/lh.obj", simFileName="lh_simplified.vtk", basePosition = [-0.5,1,-0.3], mass = 1, useNeoHookean = 1, NeoHookeanMu = 180, NeoHookeanLambda = 600, NeoHookeanDamping = 0.1, collisionMargin = 0, useSelfCollision = 1, frictionCoeff = 0.5, repulsionStiffness = 800)
 #bunnyId2 = p.loadSoftBody("torus/lh.obj", simFileName="lh_simplified.vtk", basePosition = [2,2,0], mass = 1, useNeoHookean = 1, NeoHookeanMu = 180, NeoHookeanLambda = 600, NeoHookeanDamping = 0.1, collisionMargin = 0.006, useSelfCollision = 1, frictionCoeff = 0.5, repulsionStiffness = 800)
